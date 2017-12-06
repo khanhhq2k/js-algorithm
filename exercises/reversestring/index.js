d61d00fb9930152d09d25274d6242f510717f5b5
khanhhq2k <khanhhq2k@gmail.com>
2017-12-06 19:52:03 +0000

    reverse a string execise

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,34 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  const arr = str.split('');
+  return arr.reverse().join('');
+}
+
+//old js way using for loop
+// function reverse(str) {
+//   var reversed = "";
+//   for (var i = 0; i < str.length; i++) {
+//     reversed = str[i] + reversed;
+//   }
+//   return reversed;
+// }
+
+//es6 using for loop
+// function reverse(str) {
+//   let reversed = "";
+//   for (let character of str) {
+//     reversed = character + reversed;
+//   }
+//   return reversed;
+// }
+
+// the hard way using es6 arrow function and reduce(array) function
+// function reverse(str) {
+//   return str.split('').reduce((reversed, character) => {
+//     return character + reversed;
+//   }, '');
+// }
 
 module.exports = reverse;
